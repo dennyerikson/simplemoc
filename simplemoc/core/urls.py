f481a3d457f46448c25058e0c47532fb5b2947cb
dennyerikson <dennyeriks00on@gmail.com>
2018-07-26 10:58:23 -0300

    ajustes

--- simplemoc/core/urls.py
+++ simplemoc/core/urls.py
@@ -3,4 +3,4 @@ from django.conf.urls import url, include, patterns
 urlpatterns = patterns ('simplemoc.core.views',
     url(r'^$', 'home', name='home'),
     url(r'^contato/$', 'contact', name='contact'),
-)
+) 
