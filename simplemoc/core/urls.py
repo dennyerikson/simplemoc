e4199dab66e3cbe23d39765b30dcd79d5eae31bb
dennyerikson <dennyeriks00on@gmail.com>
2018-07-31 16:33:35 -0300

    add modal enviar e-mail

--- simplemoc/core/urls.py
+++ simplemoc/core/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
+import simplemoc
 
 urlpatterns = [
-    url(r'^$', 'simplemoc.core.views.home', name='home'),
-    url(r'^contato/$', 'simplemoc.core.views.contact', name='contact'),
+    # url(r'^$', 'simplemoc.core.views.home', name='home'),
+    url(r'^$', simplemoc.core.views.home, name='home'),
+    url(r'^contato/$', simplemoc.core.views.contact, name='contact'),
+    # url(r'^contato/$', 'simplemoc.core.views.contact', name='contact'),
 ]
